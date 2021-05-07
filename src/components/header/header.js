5357b6db4d0cebbda58738fb8da56a9857e92b25
Milan Radojevic <mikister2012@gmail.com>
2021-05-07 23:46:40 +0200

    Fix navbar centering

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -12,22 +12,22 @@ function Header() {
         <header className="header">
             <LangDropdown />
             <nav className="navbar">
-                <ul>
+                <ul className="navbar__left">
                     <li>
                         <Link to="/">{t('navbar.home')}</Link>
                     </li>
                     <li>
                         <Link to="/about">{t('navbar.about')}</Link>
                     </li>
-                    <li>
-                        <Link to="/">
-                            <img
-                                src={logo}
-                                className="logo"
-                                alt={t('navbar.logoAlt')}
-                            />
-                        </Link>
-                    </li>
+                </ul>
+                <Link to="/">
+                    <img
+                        src={logo}
+                        className="logo"
+                        alt={t('navbar.logoAlt')}
+                    />
+                </Link>
+                <ul className="navbar__right">
                     <li>
                         <Link to="/collab">{t('navbar.collab')}</Link>
                     </li>
