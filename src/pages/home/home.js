2f128cc4e2bfa905875e913c6ea75bd0276a84ce
Milan Radojevic <mikister2012@gmail.com>
2021-05-08 02:08:35 +0200

    Add placeholder panels

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,13 +1,45 @@
 import { Fragment, useState } from 'react';
 import ButtonGroup from '../../components/button-group/button-group';
+import Panel from '../../components/panel/panel';
 import './home.css';
 
+import gridIcon from '../../icons/grid.svg';
+import gridIconWhite from '../../icons/grid--white.svg';
+import listIcon from '../../icons/list.svg';
+import listIconWhite from '../../icons/list--white.svg';
+import { useTranslation } from 'react-i18next';
+
 function HomePage() {
+    const { t } = useTranslation(['translation']);
     const [view, selectView] = useState(0);
     const [layout, selectLayout] = useState(0);
 
+    const setView = (index) => selectView(index);
+    const setLayout = (index) => selectLayout(index);
+
     const Views = ['Overview', 'Sonda 1', 'Sonda 2', 'Sonda 3'];
-    const Layouts = ['List', 'Grid'];
+    let Layouts = [
+        <img src={listIcon} className="control_bar__icon" alt={t('todo')} />,
+        <img
+            src={gridIconWhite}
+            className="control_bar__icon"
+            alt={t('todo')}
+        />,
+    ];
+
+    if (parseInt(layout) === 0)
+        Layouts = [
+            <img
+                src={listIconWhite}
+                className="control_bar__icon"
+                alt={t('todo')}
+            />,
+            <img
+                src={gridIcon}
+                className="control_bar__icon"
+                alt={t('todo')}
+            />,
+        ];
 
     return (
         <Fragment>
@@ -16,16 +48,27 @@ function HomePage() {
                     <ButtonGroup
                         options={Views}
                         selectedOptionIndex={view}
-                        onChange={selectView}
+                        onChange={setView}
                     />
                 </div>
                 <div className="control_bar__right">
                     <ButtonGroup
                         options={Layouts}
                         selectedOptionIndex={layout}
-                        onChange={selectLayout}
+                        onChange={setLayout}
                     />
                 </div>
+            </div>
+            <div
+                className={
+                    'panels' +
+                    (parseInt(layout) === 0 ? ' panels--list' : ' panels--grid')
+                }
+            >
+                <Panel>Map</Panel>
+                <Panel>Data</Panel>
+                <Panel>...</Panel>
+                <Panel>...</Panel>
             </div>
         </Fragment>
     );
